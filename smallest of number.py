61eee10d343af3944964b74524d0796ed846e784
keerthireddyk <42573134+keerthireddyk@users.noreply.github.com>
2019-06-14 15:53:51 +0530

    Update smallest of number.py

--- smallest of number.py
+++ smallest of number.py
@@ -1,7 +1,7 @@
 from itertools import combinations
-string1,num1=map(int,input().split())
-n1=len(str(string1))
-L1=list(combinations(str(string1),n1-num1))
+str1,num1=map(int,input().split())
+n1=len(str(str1))
+L1=list(combinations(str(str1),n1-num1))
 L1=(sorted(L1))
-b1="".join(L1[0])
-print(b1)
+a1="".join(L1[0])
+print(a1)
